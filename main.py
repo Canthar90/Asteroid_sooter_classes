b811a1fe1b5675eaf170f257ffc863b7adadbc2a
Canthar90 <d.cieslak90@gmail.com>
2023-01-31 11:36:46 +0100

    Added some graphical files

--- main.py
+++ main.py
@@ -2,14 +2,12 @@ import pygame
 import sys
 
 
-
+# base setup
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1290, 720
-
 clock = pygame.time.Clock()
-
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-
+pygame.display.set_caption("Asteroid shooter V2")
 
 
 while True:
